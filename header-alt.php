aef9ddbcf6bcb7dba7702ff47d9282023c4a05d8
Ravi Kiran <ravi.kiran8844@gmail.com>
2025-08-08 11:16:14 +0530

    added new template

---
+++ header-alt.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+  <?php wp_head(); ?>
+  <style>
+    * {
+      font-family: "DM Sans", sans-serif;
+
+    }
+  </style>
+</head>
+
+<body <?php body_class(); ?>>
